666a4548455e457af8e0233a396eeb689f6cfcff
steffengroenandersen <steffengroenandersen@gmail.com>
2024-03-03 16:49:37 +0100

    Add view functionality

--- script.js
+++ script.js
@@ -11,19 +11,44 @@ function start() {
 
 /* ########## VIEW ########## */
 
+function displayBoard() {
+  console.log("displayBoard()");
+
+  for (let row = 0; row < 6; row++) {
+    for (let col = 0; col < 7; col++) {
+      const value = readFromCell(row, col);
+      const cell = document.querySelector(`[data-row="${row}"][data-col="${col}"]`);
+
+      if (value === 1) {
+        cell.style.backgroundColor = "red";
+      }
+
+      if (value === 2) {
+        cell.style.backgroundColor = "blue";
+      }
+    }
+  }
+}
+
+// Set turn
+let turn = 2;
+
 function handleBoardClick(event) {
-  console.log("Board was clicked!");
+  //console.log("Board was clicked!");
   const cell = event.target;
 
   if (!cell.classList.contains("cell")) {
-    console.log("Cell was not clicked!");
+    //console.log("Cell was not clicked!");
     return;
   }
 
   const col = cell.dataset.col;
-  const row = cell.dataset.row;
 
-  console.log("Col: " + col + " and Row: " + row);
+  // Switch turn
+  turn = turn === 1 ? 2 : 1;
+
+  addToColumn(col, turn);
+  displayBoard();
 }
 
 /* ########## MODEL ########## */
@@ -35,5 +60,32 @@ const board = [
   [0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0],
 ];
+
+function writeToCell(row, col, value) {
+  //console.log("writeToCell()");
+  board[row][col] = value;
+  console.table(board);
+}
+
+function readFromCell(row, col) {
+  console.log("readFromCell()");
+  return board[row][col];
+}
+
+function addToColumn(col, value) {
+  console.log("addToColumn()");
+
+  if (board[0][col] === 1) {
+    console.log(`Board is full at board[0][${col}]`);
+    return;
+  }
+
+  for (let row = 5; row >= 0; row--) {
+    if (board[row][col] === 0) {
+      writeToCell(row, col, value);
+      return;
+    }
+  }
+}
 
 /* ########## CONTROLLER ########## */
